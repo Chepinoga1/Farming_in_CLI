a4fa3c1ba707bba5b7614caac1ecd1dd309e63a4
Fogg1 <103594002+Chepinoga1@users.noreply.github.com>
2026-03-22 14:40:14 +0300

    add buying system

--- game_logic.py
+++ game_logic.py
@@ -31,3 +31,11 @@ def update_game(data):
         data["fields"].remove(field)
         harvest_crop(data, field["crop"])
 
+def buy_seeds(data, crop, count):
+    if int(count) <= 0 or int(count) > data["inv"][crop]["shop_seeds"] or data["balance"] < int(count) * data["inv"][crop]["cost"]:
+        return "stop"
+    data["inv"][crop]["shop_seeds"] -= int(count)
+    data["inv"][crop]["seeds"] += int(count)
+    data["balance"] -= int(count) * data["inv"][crop]["cost"]
+    print(f"Buying {count} seeds {crop}")
+    return None
--- ui.py
+++ ui.py
@@ -3,6 +3,7 @@ def show_menu(data):
     print(f"Balance: {data['balance']}")
     print("1. Plant crops")
     print("2. Inventory")
+    print("3. Shop")
     print("0. Exit")
 
 
@@ -22,8 +23,12 @@ def print_inventory(data):
         if data["inv"][list(data["inv"])[i]]["seeds"] !=0 or data["inv"][list(data["inv"])[i]]["crops"] != 0:
             print(f"Name: {list(data["inv"].keys())[i]}, Crops: {data["inv"][list(data["inv"].keys())[i]]['crops']}, Seeds: {data["inv"][list(data["inv"].keys())[i]]['seeds']}")
 
-
-
+def print_shop(data):
+    print("\n=== SHOP ===")
+    print(f"Balance: {data['balance']}")
+    for i in range((len(data["inv"]))):
+        print(f"{i + 1}. Name: {list(data["inv"].keys())[i]}, Seeds: {data["inv"][list(data["inv"].keys())[i]]['shop_seeds']}, In inventory: {data["inv"][list(data["inv"].keys())[i]]['seeds']}, Cost: {data['inv'][list(data["inv"].keys())[i]]['cost']}")
+    print("0. Back")
 
 def get_input():
     return input("> ")
